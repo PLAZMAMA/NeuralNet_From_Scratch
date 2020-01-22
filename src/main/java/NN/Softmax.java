62aedbeaf18ccefe681474e098b3ba5295758483
Maor Cohen <maorioriori@gmail.com>
2020-01-21 23:12:30 -0500

    chagned activate method accordingly to the new interface Activations

--- src/main/java/NN/Softmax.java
+++ src/main/java/NN/Softmax.java
@@ -1,23 +1,20 @@
 package NN;
+import java.lang.Math;
 
-public class Softmax implements OutputActivations{
+public class Softmax implements Activations<Double[]>{
 
     //method for softmax function
     @Override
-    public double[] activate(double[] x){
+    public Double[] activate(Double[] x){
         double[] exponents = new double[x.length];
         double exponents_sum = 0.0;
-        double[] result = new double[x.length];
+        Double[] result = new Double[x.length];
         for(int i = 0; i < x.length; i++){
-            exponents[i] = Math.exp(x[i]);
+            exponents[i] = Math.exp(x[i].doubleValue());
             exponents_sum += exponents[i];
         }
-        System.out.println(exponents_sum);
-        System.out.flush();
         for(int i = 0; i < x.length; i++){
-            System.out.println(exponents[i] / exponents_sum);
-            System.out.flush();
-            result[i] = exponents[i] / exponents_sum;
+            result[i] = new Double(exponents[i] / exponents_sum);
         }
         return(result);
     }
