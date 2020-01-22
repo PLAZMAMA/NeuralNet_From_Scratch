5ba70f48926582151c413dd662d10176f6e9565d
Maor Cohen <maorioriori@gmail.com>
2020-01-21 21:48:51 -0500

    removed the static and body of the activate method

--- src/main/java/NN/DenseActivations.java
+++ src/main/java/NN/DenseActivations.java
@@ -4,8 +4,5 @@ package NN;
 * interface for the activation functions that would be used for the hidden / dense layers
 */
 interface DenseActivations{ 
-    public static double activate(double x){
-        System.out.println("has not been overrided from the DenseActivations interface, hence it will return 0.0");
-        return(0.0);
-    }
+    public double activate(double x);
 }
