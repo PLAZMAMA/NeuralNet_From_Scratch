8acf7a5002efeb4643b92fa337bd1a0363b63319
Maor Cohen <maorioriori@gmail.com>
2020-01-21 21:45:49 -0500

    updated the activate method to public

--- src/main/java/NN/DenseActivations.java
+++ src/main/java/NN/DenseActivations.java
@@ -4,7 +4,7 @@ package NN;
 * interface for the activation functions that would be used for the hidden / dense layers
 */
 interface DenseActivations{ 
-    static double activate(double x){
+    public static double activate(double x){
         System.out.println("has not been overrided from the DenseActivations interface, hence it will return 0.0");
         return(0.0);
     }
