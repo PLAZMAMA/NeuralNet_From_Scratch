0059cfa1752626abdc1375137050502c0015070b
Maor Cohen <maorioriori@gmail.com>
2020-01-22 15:38:44 -0500

    changed activate method accordingly to the new interface Activations

---
+++ src/main/java/NN/Sigmoid.java
@@ -0,0 +1,11 @@
+package NN;
+
+import java.lang.Math;
+
+public class Sigmoid implements Activations<Double>{
+
+    //method for the sigmoid function
+    public Double activate(Double x){
+        return(new Double(1.0 / (1.0 + Math.exp(-x))));
+    }
+}
