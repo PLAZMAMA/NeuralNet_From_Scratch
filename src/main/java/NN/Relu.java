cd8ba6fa8c6a6b6b08cbed46f76ea2c81433fb10
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:18:39 -0500

    added comments to the Relu class

--- src/main/java/NN/Relu.java
+++ src/main/java/NN/Relu.java
@@ -6,6 +6,7 @@ public class Relu implements Activations<Double>{
     
     //method for the relu function
     public Double activate(Double x){
+        //if x(given value) is less than 0, then the function will return 0. otherwise it will return x(given value)
         return(new Double(Math.max(0, x.doubleValue())));
     }
 }
