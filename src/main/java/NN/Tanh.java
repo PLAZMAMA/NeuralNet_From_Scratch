fae2692633baeede6476d1a5e528854b062771d6
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:23:45 -0500

    edited comment to the Tanh class

--- src/main/java/NN/Tanh.java
+++ src/main/java/NN/Tanh.java
@@ -6,7 +6,14 @@ public class Tanh implements Activations<Double>{
     //method for the tanh function
     @Override
     public Double activate(Double x){
+        // store x's (given value) double value as a double in temp
         double temp = x.doubleValue();
+
+        /*
+        returns the exponenet of temp minus the exponent of negetive temp,
+        all divided by the exponent of temp plus the exponent of negetive temp
+        and casts it all into a Double(class/wrapper of double)
+        */
         return(new Double((Math.exp(temp) - Math.exp(-temp)) / (Math.exp(temp) + Math.exp(-temp))));
     }
 }
