b11587ee53ad3c195ea54f3c4a6521488b49e9bc
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:17:11 -0500

    added comments to the Sigmoid class

--- src/main/java/NN/Sigmoid.java
+++ src/main/java/NN/Sigmoid.java
@@ -6,6 +6,7 @@ public class Sigmoid implements Activations<Double>{
 
     //method for the sigmoid function
     public Double activate(Double x){
+        //returns 1 devided by 1 plus the negetive natural base (e) to the power of the negetive of the given value
         return(new Double(1.0 / (1.0 + Math.exp(-x.doubleValue()))));
     }
 }
