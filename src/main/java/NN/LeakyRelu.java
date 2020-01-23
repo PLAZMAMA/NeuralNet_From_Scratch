8ddf4e2081eb98a2235536a746e2a85212356607
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:26:59 -0500

    edited comment to the LeakyRelu class

--- src/main/java/NN/LeakyRelu.java
+++ src/main/java/NN/LeakyRelu.java
@@ -6,6 +6,10 @@ public class LeakyRelu implements Activations<Double>{
     //method for the leaky relu function
     @Override
     public Double activate(Double x){
+        /*
+        returns x if x (given value) is bigger than 0, 
+        otherwise it returns the x (given value) times 0.01 then all casted to a Double (class/wrapper of double)
+        */
         if (x < 0.0){
         double temp = x.doubleValue();
         x = new Double(0.01 * temp);
