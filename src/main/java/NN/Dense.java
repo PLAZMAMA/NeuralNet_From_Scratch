1b10b65bb41dbe379a1f00ba4385c112c371d06a
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:13:46 -0500

    added comments to the Dense class

--- src/main/java/NN/Dense.java
+++ src/main/java/NN/Dense.java
@@ -1,42 +1,43 @@
 package NN;
-import java.lang.reflect.Method;
 
 import NN.Activations;
 
-/*
-* this class creates dense neural net layer
-*/
-public class Dense{
+//this class creates dense neural net layer
+public class Dense extends Layers{
     //instance variables
-    double[] biases;
-    double[][] nodes;
-    Method activation;
+    double bias;
+    double[] nodes;
+    Activations<Double> activation;
+    
 
-    /*
-    * constructor method for the Dense class
-    */
-    public void Dense(int n_nodes, String activation){
-        this.nodes = new double[n_nodes][n_nodes];
-        this.biases = new double[n_nodes];
-        this.activation = 
-        for(int i = 0; i < n_nodes; i++){
-            this.biases[i] = 0.0;
-        }
+    
+    //constructor for the Dense class
+    Dense(int n_nodes, Activations<Double> activation){
+        this.nodes = new double[n_nodes];
+        this.bias = 0.0;
+        this.activation = activation;
 
 
     }
     
     /*
-    * calculate the layer's nodes with the given weights, last layers nodes and biases.
-    * it sums up all the given weights and biases, then adds the bias, lastly it puts it through the activation function 
+    calculate the layer's nodes with the given weights and last layers nodes.
+    it sums up all the given weights and biases, then adds the bias, lastly it puts it through the activation function .
+    the function cahnges this.nodes to the nodes with the activation function added
     */
-    public double[] calculate_nodes(double[] last_layer_vals, double[][] weights){
+    public void calculate_nodes(double[] last_layer_vals, double[][] weights){
+        double sum;
         for(int i = 0; i < weights.length; i++){
-            for(int j = 0; j < last_layer_vals.length; i++){
 
+            //gets the sum of the last layer's nodes vals times the corresponding/connected weight
+            sum = 0.0;
+            for(int j = 0; j < weights[i].length; i++){
+                sum += last_layer_vals[j] * weights[i][j];
             }
+
+            //adds the bias to the sum, puts in through the given(in the constructor) activation function and stores it in this.nodes
+            this.nodes[i] = this.activation.activate(sum + this.bias);
         }
-        return(this.nodes);
     }
 
     
