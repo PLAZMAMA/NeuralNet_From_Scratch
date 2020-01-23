290010b229c6a6016a3d0fbda6855f3b99814b9a
Maor Cohen <maorioriori@gmail.com>
2020-01-22 21:12:45 -0500

    added comments to the softmax class

--- src/main/java/NN/Softmax.java
+++ src/main/java/NN/Softmax.java
@@ -6,13 +6,18 @@ public class Softmax implements Activations<Double[]>{
     //method for softmax function
     @Override
     public Double[] activate(Double[] x){
+        //initializes all the variables
         double[] exponents = new double[x.length];
         double exponents_sum = 0.0;
         Double[] result = new Double[x.length];
+
+        //gets each values exponent, stores it in an array and adds it to the exponent sum
         for(int i = 0; i < x.length; i++){
             exponents[i] = Math.exp(x[i].doubleValue());
             exponents_sum += exponents[i];
         }
+
+        //iterates over each values exponents, devides it by the exponents sum and then stores it in result
         for(int i = 0; i < x.length; i++){
             result[i] = new Double(exponents[i] / exponents_sum);
         }
