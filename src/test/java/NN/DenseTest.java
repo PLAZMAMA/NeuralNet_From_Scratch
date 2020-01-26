755b4a96a8581ff23132b79c7b756d8bdb0c3e52
Maor Cohen <maorioriori@gmail.com>
2020-01-25 19:08:31 -0500

    updated the test methods from public to pubic

--- src/test/java/NN/DenseTest.java
+++ src/test/java/NN/DenseTest.java
@@ -19,19 +19,19 @@ public class DenseTest{
     private Dense dense;
 
     @Before
-    private void init(){
+    public void init(){
         Sigmoid sig = new Sigmoid();
         this.dense = new Dense(sig, 5);
     }
 
     @Test
-    private void test_constuctor(){
+    public void test_constuctor(){
         double expected = 0.0;
         assertEquals(expected, this.dense.bias, 0.00001);
     }
 
     @Test
-    private void test_get_layer_nodes(){
+    public void test_get_layer_nodes(){
         this.dense.bias = 0.264;
         double[] last_layer_vals = {0.5, 0.4, 0.7};
         double[][] weights = {{0.2, -0.6, 1.8, 0.3, 0.9}, {0.1, 1.5, -1.3, 1.2, -1.0}, {1.2, 1.0, -0.4, -0.1, 0.1}};
