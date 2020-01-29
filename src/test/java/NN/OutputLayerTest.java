8e34269a772fb6e5f2f85c0c313969ced4f20d0b
Maor Cohen <maorioriori@gmail.com>
2020-01-28 19:05:06 -0500

    updated test_calculate_nodes

--- src/test/java/NN/OutputLayerTest.java
+++ src/test/java/NN/OutputLayerTest.java
@@ -23,11 +23,7 @@ public class OutputLayerTest{
         double[] last_layer_vals = {0.4, 0.9, 0.2, 0.5, 0.8};
         double[][] weights = {{0.1, 0.5, 0.6}, {0.5, 0.9, 0.7}, {-1.2, -1.0, -0.9}, {1.0, -0.9, -0.6}, {0.8, -0.8, 1.3}};
         double[] expected = {0.44862003225352, 0.084451404658485, 0.466928563088};
-        double[] actual = new double[3];
         output.calculate_nodes(last_layer_vals, weights);
-        for(int i = 0; i < actual.length; i++){
-            actual[i] = output.nodes[i].doubleValue();
-        }
-        assertArrayEquals(expected, actual, 0.00001);
+        assertArrayEquals(expected, output.nodes, 0.00001);
     }
 }
