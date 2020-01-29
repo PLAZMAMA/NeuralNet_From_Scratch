b525fae8f82b287d921e8d771933726829341a1c
Maor Cohen <maorioriori@gmail.com>
2020-01-29 09:17:20 -0500

    added test_type method and added messages to the test methods

--- src/test/java/NN/OutputLayerTest.java
+++ src/test/java/NN/OutputLayerTest.java
@@ -1,6 +1,7 @@
 package NN;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -24,6 +25,12 @@ public class OutputLayerTest{
         double[][] weights = {{0.1, 0.5, 0.6}, {0.5, 0.9, 0.7}, {-1.2, -1.0, -0.9}, {1.0, -0.9, -0.6}, {0.8, -0.8, 1.3}};
         double[] expected = {0.44862003225352, 0.084451404658485, 0.466928563088};
         output.calculate_nodes(last_layer_vals, weights);
-        assertArrayEquals(expected, output.nodes, 0.00001);
+        assertArrayEquals("calculate_nodes is not working as intended or there is not acess to the nodes of output", expected, output.nodes, 0.00001);
+    }
+
+    @Test
+    public void test_type(){
+        String expected = "Output";
+        assertTrue("type is not correct", this.output.type.equals(expected));
     }
 }
