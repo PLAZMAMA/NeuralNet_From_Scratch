b2e1a4b68629dec5022eb4028adcc7a2b685632f
Maor Cohen <maorioriori@gmail.com>
2020-01-28 19:11:54 -0500

    updated calculate_nodes due to changing the type of nodes from Double to double

--- src/main/java/NN/OutputLayer.java
+++ src/main/java/NN/OutputLayer.java
@@ -7,13 +7,15 @@ import java.lang.Double;
 public class OutputLayer extends Layers{
     //instance variables
     Activations<Double[]> activation;
-    Double[] nodes;
+    double[] nodes;
+
 
 
     //constructor method for the outputlayer class
     OutputLayer(Activations<Double[]> activation, int n_nodes){
         this.activation = activation;
-        this.nodes = new Double[n_nodes];
+        this.nodes = new double[n_nodes];
+        this.type = "Output";
     }
 
     @Override
@@ -27,9 +29,15 @@ public class OutputLayer extends Layers{
             for(int row = 0; row < weights.length; row++){
                 sum += last_layer_vals[row] * weights[row][column];
             }
+            //stores the sum of each node in a temporary Double array
             temp_nodes_vals[column] = new Double(sum);
         }
-        this.nodes = this.activation.activate(temp_nodes_vals);
+        //the temporary Double array is put into a activation function(in this case probably softmax),
+        //then the output of the activation function is stored in this.nodes/nodes by dumping the temp array into the 
+        temp_nodes_vals = this.activation.activate(temp_nodes_vals);
+        for(int i = 0; i < temp_nodes_vals.length; i++){
+            this.nodes[i] = temp_nodes_vals[i].doubleValue();
+        }
     }
 
 
