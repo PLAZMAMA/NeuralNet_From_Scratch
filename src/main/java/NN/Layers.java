21bcbd79dd1f182e337262b7890c0da8296fc03a
Maor Cohen <maorioriori@gmail.com>
2020-01-29 12:37:59 -0500

    added nodes into the abstract class

--- src/main/java/NN/Layers.java
+++ src/main/java/NN/Layers.java
@@ -3,6 +3,7 @@ package NN;
 abstract class Layers{
     //instance variable
     String type;
+    double[] nodes;
 
     //abstract methods
     public abstract void calculate_nodes(double[] last_layer_vals, double[][] weights);
