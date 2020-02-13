8a9d87fc15ed04206045fd0a2034975f9a2634f7
Maor Cohen <maorioriori@gmail.com>
2020-02-13 18:11:25 -0500

    added deriv_activate method

--- src/main/java/NN/Sigmoid.java
+++ src/main/java/NN/Sigmoid.java
@@ -9,4 +9,10 @@ public class Sigmoid implements Activations<Double>{
         //returns 1 devided by 1 plus the negetive natural base (e) to the power of the negetive of the given value
         return(new Double(1.0 / (1.0 + Math.exp(-x.doubleValue()))));
     }
+
+    //method for the derivative of the sigmoid function
+    public Double deriv_activate(Double x){
+        //returns the derivative of the sigmoid function
+        return(this.activate(x) - (1 - this.activate(x)));
+    }
 }
