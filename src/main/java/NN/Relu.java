41ec54b7b2e575e453b932d750e573c67eeccabc
Maor Cohen <maorioriori@gmail.com>
2020-02-13 18:10:57 -0500

    added deriv_activate method

--- src/main/java/NN/Relu.java
+++ src/main/java/NN/Relu.java
@@ -9,4 +9,19 @@ public class Relu implements Activations<Double>{
         //if x(given value) is less than it returns 0, then the function will return 0. otherwise it will return x(given value)
         return(new Double(Math.max(0, x.doubleValue())));
     }
+
+    //method for the derivative of the relu function
+    public Double deriv_activate(Double x){
+        /*
+        if x is bigger than 0 then it returns 1, if x is smaller than 0 it returns 0.
+        However if x is 0 it is not clear what should be return so I decided on returning a half(0.5)
+        */
+        if(x > 0.0){
+            return(1.0);
+        }else if(x < 0.0){
+            return(0.0);
+        }else{
+            return(0.5);
+        }
+    }
 }
