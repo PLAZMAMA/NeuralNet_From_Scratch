8d5bd00769b22723c693050ede80b3014f80bf19
Maor Cohen <maorioriori@gmail.com>
2020-02-14 16:51:50 -0500

    added the test_deriv methods for each activation besides softmax

--- src/test/java/NN/ActivationsTest.java
+++ src/test/java/NN/ActivationsTest.java
@@ -14,9 +14,17 @@ public class ActivationsTest{
     @Test
     public void test_sigmoid(){
         Activations<Double> act = (Activations<Double>)new Sigmoid();
-        assertEquals("test 1 of failed", 0.9933071491, act.activate(5.0), 0.00001);
-        assertEquals("test 2 of failed", 0.006692850924, act.activate(-5.0), 0.00001);
-        assertEquals("test 3 of failed", 0.5, act.activate(0.0), 0.00001);
+        assertEquals("test 1 failed", 0.9933071491, act.activate(5.0), 0.00001);
+        assertEquals("test 2 failed", 0.006692850924, act.activate(-5.0), 0.00001);
+        assertEquals("test 3 failed", 0.5, act.activate(0.0), 0.00001);
+    }
+
+    @Test
+    public void test_deriv_sigmoid(){
+        Activations<Double> act = (Activations<Double>) new Sigmoid();
+        assertEquals("test 1 failed", 0.235004, act.deriv_activate(0.5), 0.00001);
+        assertEquals("test 2 failed", 0.2392912, act.deriv_activate(-0.42), 0.00001);
+
     }
 
     @Test
@@ -24,7 +32,14 @@ public class ActivationsTest{
         Activations<Double> act = (Activations<Double>)new Tanh();
         assertEquals("test 1 failed", 0.9999092043, act.activate(5.0), 0.00001);
         assertEquals("test 2 failed", -0.9999092043, act.activate(-5.0), 0.00001);
-        assertEquals("test 3 of failed", 0.0, act.activate(0.0), 0.00001);
+        assertEquals("test 3 failed", 0.0, act.activate(0.0), 0.00001);
+    }
+
+    @Test
+    public void test_deriv_tanh(){
+        Activations<Double> act = (Activations<Double>) new Tanh();
+        assertEquals("test 1 failed", 0.78644773296, act.deriv_activate(0.5), 0.00001);
+        assertEquals("test 2 failed", 0.604176365869, act.deriv_activate(-0.74), 0.00001);
     }
 
     @Test
@@ -35,12 +50,26 @@ public class ActivationsTest{
         assertEquals("test 3 failed", 0.0, act.activate(0.0), 0.00001);
     }
 
+    @Test 
+    public void test_deriv_relu(){
+        Activations<Double> act = (Activations<Double>) new Relu();
+        assertEquals("test 1 failed", 1.0, act.deriv_activate(0.5), 0.01);
+        assertEquals("test 2 failed", 0.0, act.deriv_activate(-0.5), 0.01);
+    }
+
     @Test
     public void test_leaky_relu(){
         Activations<Double> act = (Activations<Double>)new LeakyRelu();
         assertEquals("test 1 failed", 5.0, act.activate(5.0), 0.00001);
         assertEquals("test 2 failed", -0.05, act.activate(-5.0), 0.00001);
         assertEquals("test 3 failed", 0, act.activate(0.0), 0.00001);
+    }
+
+    @Test
+    public void test_deriv_leaky_relu(){
+        Activations<Double> act = (Activations<Double>) new LeakyRelu();
+        assertEquals("test 1 failed", 0.01,act.deriv_activate(-1.0), 0.01);
+        assertEquals("test 2 failed", 1.0, act.deriv_activate(1.0), 0.01);
     }
 
     @Test
@@ -55,6 +84,11 @@ public class ActivationsTest{
         }
 
         assertArrayEquals("the softmax function does not work as intended", expected, actual, 0.00001);
+    }
+
+    @Test
+    public void test_deriv_softmax(){
+        Activations<Double[]> act = (Activations<Double[]>) new Softmax();
     }
 
 
