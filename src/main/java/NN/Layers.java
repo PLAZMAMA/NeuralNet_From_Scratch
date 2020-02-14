7ef5e7155a045a9b0c7a88e48f2aead4df536232
Maor Cohen <maorioriori@gmail.com>
2020-02-13 21:00:10 -0500

    changed the biases from a single value for each layer(Dense layer) to each node(making it an array/vector insthead of a single value

--- src/main/java/NN/Layers.java
+++ src/main/java/NN/Layers.java
@@ -4,6 +4,7 @@ abstract class Layers{
     //instance variable
     String type;
     double[] nodes;
+    double[] biases;
 
     //abstract methods
     public abstract void calculate_nodes(double[] last_layer_vals, Double[][] weights);
