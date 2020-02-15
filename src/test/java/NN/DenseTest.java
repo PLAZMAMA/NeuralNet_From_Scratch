5807c85417c1e2bc3ee0789779c5c5ef810a74b3
Maor Cohen <maorioriori@gmail.com>
2020-02-15 18:55:38 -0500

    changed the method test_constructor to test_bias_initialization and inside that method changed the expected to an array of biases due to the classes bias changing into biases. Changed inside test_calculate_nodes the test to match the new arcitecture of the biases and weights

--- src/test/java/NN/DenseTest.java
+++ src/test/java/NN/DenseTest.java
@@ -23,18 +23,21 @@ public class DenseTest{
     }
 
     @Test
-    public void test_constuctor(){
-        double expected = 0.0;
-        assertEquals("the bias was not initiallized correctly or there is not acess to the bias of dense", expected, this.dense.bias, 0.00001);
+    public void test_bias_initialization(){
+        double[] expected = {0.0, 0.0, 0.0, 0.0, 0.0};
+        assertArrayEquals("the bias was not initiallized correctly or there is not acess to the bias of dense", expected, this.dense.biases, 0.00001);
     }
 
     @Test
     public void test_calculate_nodes(){
-        this.dense.bias = 0.264;
+        double[] new_biases = {0.24, 0.13, 0.64, 0.93, 0.78};
+        for(int i = 0; i < this.dense.biases.length; i++){
+            this.dense.biases[i] = new_biases[i];
+        }
         double[] last_layer_vals = {0.5, 0.4, 0.7};
-        Double[][] weights = {{0.2, -0.6, 1.8, 0.3, 0.9}, {0.1, 1.5, -1.3, 1.2, -1.0}, {1.2, 1.0, -0.4, -0.1, 0.1}};
+        Double[][] weights = {{0.2, 0.1, 1.2}, {-0.6, 1.5, 1.0}, {1.8, -1.3, -0.4}, {0.3, 1.2, -0.1}, {0.9, -1.0, 0.1}};
         this.dense.calculate_nodes(last_layer_vals, weights);
-        double[] expected = {0.776259, 0.7797139, 0.5900083, 0.6950847, 0.59483749};
+        double[] expected = {0.77206354942678, 0.755838899094, 0.67699585623, 0.81607827258, 0.7109495026250}; //these values havn't been put throught the activation function
         assertArrayEquals("the calculate_nodes method does not work or there is not access to the nodes of dense", expected, this.dense.nodes, 0.0001);
     }
 }
