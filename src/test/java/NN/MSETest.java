7e6bdcc5de8e7a5469a45edc80db05ce384b7a10
Maor Cohen <maorioriori@gmail.com>
2020-02-18 11:56:31 -0500

    created test_deriv_calculate method

--- src/test/java/NN/MSETest.java
+++ src/test/java/NN/MSETest.java
@@ -22,5 +22,18 @@ public class MSETest{
         double expected = 0.063333;
         assertEquals(expected, actual, 0.0001);
     }
-
+    
+    @Test
+    public void test_deriv_calculate(){
+        double output = 0.15;
+        double label = 0.0;
+        double actual = this.mse.deriv_calculate(output, label);
+        double expected = 0.3;
+        assertEquals("test 1 failed", expected, actual, 0.0001);
+        output = 0.95;
+        label = 1.0;
+        actual = this.mse.deriv_calculate(output, label);
+        expected = -0.1;
+        assertEquals("test 2 failed", expected, actual, 0.0001);
+    }
 }
