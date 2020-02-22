6d9a7d04f24192f4111fe3580efd147e68592326
Maor Cohen <maorioriori@gmail.com>
2020-02-22 18:50:31 -0500

    changed the weights hashmap into a tensor(aka 3 dementional matrix)

--- src/main/java/NN/Model.java
+++ src/main/java/NN/Model.java
@@ -4,16 +4,20 @@ import java.util.HashMap;
 import java.util.Random;
 import java.lang.Math;
 
+import NN.MSE;
+
 
 public class Model{
     //instance variables
     Layers[] layers;
-    HashMap<String,Double[][]> weights;
+    double[][][] weights;
+    MSE cost;
 
     Model(Layers... layers){
         this.layers = layers;
-        this.weights = new HashMap<>(this.layers.length);
-        Double[][] current_layer_weights;
+        this.weights = new double[this.layers.length][][];
+        this.cost = new MSE();
+        double[][] current_layer_weights;
 
         /*
         initializing all the weights
@@ -25,7 +29,7 @@ public class Model{
         */
         for(int i = 1; i < this.layers.length; i++){
             current_layer_weights = create_weights(this.layers[i-1].nodes.length, this.layers[i].nodes.length);
-            this.weights.put("layer_" + i, current_layer_weights);
+            this.weights[i] = current_layer_weights;
         }
         
     }
@@ -36,9 +40,9 @@ public class Model{
     *and multiply it by the square root of 2 divided by the last layer size plus the current layer size
     *to try to reduce the vanishing and exploding gradient problems.
     */
-    public static Double[][] create_weights(int last_layer_size, int current_layer_size){
+    public static double[][] create_weights(int last_layer_size, int current_layer_size){
         Random r = new Random();
-        Double[][] weights = new Double[current_layer_size][last_layer_size];
+        double[][] weights = new double[current_layer_size][last_layer_size];
         for(int row = 0; row < current_layer_size; row++){
             for(int column = 0; column < last_layer_size; column++){
                 weights[row][column] = r.nextGaussian() * Math.sqrt(2.0 / last_layer_size + current_layer_size);
@@ -50,11 +54,16 @@ public class Model{
     //calculates each layer's (beside the input layer since it just the data technically) nodes in sequetial order from the first (not counting the input) to the output
     public void feed_forward(){
         for(int i = 1; i < this.layers.length; i++){
-            layers[i].calculate_nodes(this.layers[i-1].nodes, weights.get("layer_" + i));
+            layers[i].calculate_nodes(this.layers[i-1].nodes, weights[i]);
         }
     }
 
-    public void back_propagate(){
+    //this method will change the weights and biases based on the loss of the cost function
+    public void back_propagate(double output, double label){
+        
+    }
+
+    public double[][] calculate_weights(double[] dc_dal, int layer){
 
     }
 }
